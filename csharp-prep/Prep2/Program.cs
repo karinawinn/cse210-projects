004fdd731912a5c9840d3d8b39a5fcc2ee5124d7
Karina Winn <win22019@byui.edu>
2024-01-13 11:37:43 -0700

    Completed Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
+        string letter = " ";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is a {letter}.");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else if (gradePercentage < 70)
+        {
+            Console.WriteLine("You didn't pass the class. Better luck next time!");
+        }
     }
 }
