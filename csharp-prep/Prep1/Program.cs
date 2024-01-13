4e705580a82a08066b9360623e15951baefab519
Karina Winn <win22019@byui.edu>
2024-01-12 17:04:38 -0700

    Ammemded String Inerpolation Line

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,6 @@ class Program
         string first = Console.ReadLine();
         Console.Write("What is your last name?");
         string last = Console.ReadLine();
-        Console.WriteLine($"Your name is {first}, {last} {first}.");
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
