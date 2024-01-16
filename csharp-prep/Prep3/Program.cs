50da68be2f597578816b37c96b145e087001e97c
Karina Winn <win22019@byui.edu>
2024-01-16 14:37:32 -0700

    Completed Prep 3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        string again = "yes";
+        while (again == "yes")
+        {
+            Console.Write("What is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess > number) 
+            {
+                Console.WriteLine("Lower");
+                again = "yes";
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                again = "yes";
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You guessed the number!");
+                again = "no";
+            }
+        }
+
     }
 }
