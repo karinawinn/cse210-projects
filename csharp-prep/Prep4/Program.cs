16f9df271b6bf9d6e469b3e334a528e4a7e49bf9
Karina Winn <win22019@byui.edu>
2024-01-16 16:18:42 -0700

    Completed Prep 4 Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+        int input = 1;
+        List<int> numbers = new List<int>();
+        while (input != 0)
+        {
+            Console.Write("Enter number:");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        int sum = 0;
+        int max = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        int average = sum / numbers.Count();
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The greatest number is {max}.");
     }
 }
