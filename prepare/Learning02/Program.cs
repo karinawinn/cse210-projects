c6aa57df047dffcb2ee0fcf10e1dfdb23c14120a
Karina Winn <win22019@byui.edu>
2024-01-22 12:45:49 -0700

    Completed abstraction assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job("Del Taco", "Manager", 1706, 2020);
+
+        Job job2 = new Job("Google", "CTO", 2020, 2024);
+
+        Resume myResume = new Resume();
+        myResume._name = "Karina Winn";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
